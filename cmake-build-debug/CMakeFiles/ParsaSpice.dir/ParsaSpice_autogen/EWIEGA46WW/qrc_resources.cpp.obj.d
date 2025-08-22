954b659069b28514106b67074fee4fbf4f2c1402
parsapaktinat <parsapaktinat63@gmail.com>
2025-08-22 20:29:13 +0330

    Going to push

---
+++ cmake-build-debug/CMakeFiles/ParsaSpice.dir/ParsaSpice_autogen/EWIEGA46WW/qrc_resources.cpp.obj.d
@@ -0,0 +1,2 @@
+CMakeFiles/ParsaSpice.dir/ParsaSpice_autogen/EWIEGA46WW/qrc_resources.cpp.obj: \
+ C:\Users\parsa\Documents\university\Programming\ and\ linux\ltspice_simulator_cpp_sdl2\cmake-build-debug\ParsaSpice_autogen\EWIEGA46WW\qrc_resources.cpp
